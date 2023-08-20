a2bd8e5c6a659240073b55e207984327246c3904
Zack Pyle <zack@pyledigital.com>
2023-08-20 07:33:21 -0500

    Update version to reflect new versioning in preparation for 1.0 release

--- bb-class-dropdown.php
+++ bb-class-dropdown.php
@@ -9,7 +9,7 @@
  * @wordpress-plugin
  * Plugin Name: Beaver Builder Class Dropdown
  * Description: Adds user defined CSS classes to dropdown below the Beaver Builder class input in the Advanced tab
- * Version:     1.7
+ * Version:     v0.7-beta
  * Author:      PYLE/DIGITAL
  * Text Domain: textdomain
  * License:     GPL-2.0+
@@ -17,7 +17,7 @@
  */
 
 
-define( 'BBCLASSDROPDOWN_VERSION', '1.7' );
+define( 'BBCLASSDROPDOWN_VERSION', '0.7' );
 define( 'BBCLASSDROPDOWN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'BBCLASSDROPDOWN_FILE', __FILE__ );
 define( 'BBCLASSDROPDOWN_URL', plugins_url( '/', __FILE__ ) );
