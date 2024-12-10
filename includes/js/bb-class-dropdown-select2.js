86758c84d8b0fb922498af6296d2776d4cb30459
Zack Pyle <zack@pyledigital.com>
2024-12-10 07:55:39 -0600

    Update bb-class-dropdown-select2.js

--- includes/js/bb-class-dropdown-select2.js
+++ includes/js/bb-class-dropdown-select2.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
     // Turn <select> into select 2
     FLBuilder.addHook('settings-form-init', function(){
         $('.fl-builder-settings:visible .fl-text-field-add-value').select2({
-            placeholder: "- Choose from predefined classes -",
+            placeholder: "- Choose from utility classes -",
             width: '50%'
         });
     });
